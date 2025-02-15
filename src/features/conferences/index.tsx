e41508777025c8be39d2b1a7f58a9f0fdbb2b7ad
21120321 <gamefun206@gmail.com>
2025-02-15 14:41:41 +0700

    init from template

---
+++ src/features/conferences/index.tsx
@@ -0,0 +1,8 @@
+export default function Conferences() {
+  return (
+    <div>
+      <h1>Conferences</h1>
+      <p>Conferences page content</p>
+    </div>
+  );
+}
---
+++ src/routes/_authenticated/conferences/index.lazy.tsx
@@ -0,0 +1,7 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+import Conferences from '@/features/conferences'
+export const Route = createLazyFileRoute('/_authenticated/conferences/')({
+  component: Conferences,
+})
+
+
